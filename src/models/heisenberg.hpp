13359bcbc5975c516ea029638acbb7516827da8c
Egor Chernook <egor.chernook@yandex.ru>
2023-02-25 20:03:40 +0600

    добавил методы is_almost_equals

--- src/models/heisenberg.hpp
+++ src/models/heisenberg.hpp
@@ -3,192 +3,197 @@
 
 #include <iostream>
 
-#include "../random/random.hpp"
 #include "../random/mersenne.hpp"
+#include "../random/random.hpp"
 
 namespace qss::inline models::heisenberg
 {
-    struct magn
+struct magn
+{
+    double x = 0.0;
+    double y = 0.0;
+    double z = 0.0;
+
+    magn &operator+=(const magn &rhs) noexcept
     {
-        double x = 0.0;
-        double y = 0.0;
-        double z = 0.0;
-
-        magn &operator+=(const magn &rhs) noexcept
-        {
-            x += rhs.x;
-            y += rhs.y;
-            z += rhs.z;
-            return *this;
-        }
-        magn &operator-=(const magn &rhs) noexcept
-        {
-            x -= rhs.x;
-            y -= rhs.y;
-            z -= rhs.z;
-            return *this;
-        }
-        magn &operator*=(const double &rhs) noexcept
-        {
-            x *= rhs;
-            y *= rhs;
-            z *= rhs;
-            return *this;
-        }
-        magn &operator/=(const double &rhs) noexcept
-        {
-            x /= rhs;
-            y /= rhs;
-            z /= rhs;
-            return *this;
-        }
-    };
-    struct spin
+        x += rhs.x;
+        y += rhs.y;
+        z += rhs.z;
+        return *this;
+    }
+    magn &operator-=(const magn &rhs) noexcept
     {
-        using magn_t = magn;
-        double x = 1.0;
-        double y = 0.0;
-        double z = 0.0;
-
-        template <Random random_t = qss::random::mersenne::random_t<>>
-        static spin generate() noexcept
-        {
-            static random_t rand{qss::random::get_seed()};
-            const double phi = rand.get_angle_2pi();
-            const double eta = rand.get_angle_pi();
-            const double sinus_eta = std::sin(eta / 2);
-
-            spin result{};
-            result.x = sinus_eta * std::cos(phi / 2);
-            result.y = sinus_eta * std::sin(phi / 2);
-            result.z = std::cos(eta / 2);
-
-            return result;
-        }
-
-        operator magn_t() const noexcept
-        {
-            return magn_t{x, y, z};
-        }
-    };
-
-    inline magn operator+(const spin &lhs, const spin &rhs) noexcept
+        x -= rhs.x;
+        y -= rhs.y;
+        z -= rhs.z;
+        return *this;
+    }
+    magn &operator*=(const double &rhs) noexcept
     {
-        magn result{};
-        result.x = lhs.x + rhs.x;
-        result.y = lhs.y + rhs.y;
-        result.z = lhs.z + rhs.z;
+        x *= rhs;
+        y *= rhs;
+        z *= rhs;
+        return *this;
+    }
+    magn &operator/=(const double &rhs) noexcept
+    {
+        x /= rhs;
+        y /= rhs;
+        z /= rhs;
+        return *this;
+    }
+};
+struct spin
+{
+    using magn_t = magn;
+    double x = 1.0;
+    double y = 0.0;
+    double z = 0.0;
+
+    template <Random random_t = qss::random::mersenne::random_t<>> static spin generate() noexcept
+    {
+        static random_t rand{qss::random::get_seed()};
+        const double phi = rand.get_angle_2pi();
+        const double eta = rand.get_angle_pi();
+        const double sinus_eta = std::sin(eta / 2);
+
+        spin result{};
+        result.x = sinus_eta * std::cos(phi / 2);
+        result.y = sinus_eta * std::sin(phi / 2);
+        result.z = std::cos(eta / 2);
+
         return result;
     }
-    inline magn operator+(const magn &lhs, const magn &rhs) noexcept
+
+    operator magn_t() const noexcept
     {
-        magn result{};
-        result.x = lhs.x + rhs.x;
-        result.y = lhs.y + rhs.y;
-        result.z = lhs.z + rhs.z;
-        return result;
+        return magn_t{x, y, z};
     }
-    inline magn operator-(const spin &lhs, const spin &rhs) noexcept
-    {
-        magn result{};
-        result.x = lhs.x - rhs.x;
-        result.y = lhs.y - rhs.y;
-        result.z = lhs.z - rhs.z;
-        return result;
-    }
-    inline magn operator-(const magn &lhs, const magn &rhs) noexcept
-    {
-        magn result{};
-        result.x = lhs.x - rhs.x;
-        result.y = lhs.y - rhs.y;
-        result.z = lhs.z - rhs.z;
-        return result;
-    }
-    inline double scalar_multiply(const spin &lhs, const spin &rhs) noexcept
-    {
-        double sum = 0.0;
-        sum += lhs.x * rhs.x;
-        sum += lhs.y * rhs.y;
-        sum += lhs.z * rhs.z;
-        return sum;
-    }
-    inline double scalar_multiply(const magn &lhs, const magn &rhs) noexcept
-    {
-        double sum = 0.0;
-        sum += lhs.x * rhs.x;
-        sum += lhs.y * rhs.y;
-        sum += lhs.z * rhs.z;
-        return sum;
-    }
-    inline magn operator*(const double &lhs, const spin &rhs) noexcept
-    {
-        magn result;
-        result.x = lhs * rhs.x;
-        result.y = lhs * rhs.y;
-        result.z = lhs * rhs.z;
-        return (result);
-    }
-    inline magn operator*(const double &lhs, const magn &rhs) noexcept
-    {
-        magn result;
-        result.x = lhs * rhs.x;
-        result.y = lhs * rhs.y;
-        result.z = lhs * rhs.z;
-        return (result);
-    }
-    inline magn operator*(const spin &lhs, const double &rhs) noexcept
-    {
-        return (rhs * lhs);
-    }
-    inline magn operator*(const magn &lhs, const double &rhs) noexcept
-    {
-        return (rhs * lhs);
-    }
-    inline magn operator/(const spin &lhs, const double &rhs) noexcept
-    {
-        magn result{};
-        result.x = lhs.x / rhs;
-        result.y = lhs.y / rhs;
-        result.z = lhs.z / rhs;
-        return result;
-    }
-    inline magn operator/(const magn &lhs, const double &rhs) noexcept
-    {
-        magn result{};
-        result.x = lhs.x / rhs;
-        result.y = lhs.y / rhs;
-        result.z = lhs.z / rhs;
-        return result;
-    }
-    
-    inline std::string to_string(const magn &data) noexcept {
-        std::ostringstream stream{};
-        stream << "( " << data.x << " , " << data.y << " , " << data.z << " )";
-        return stream.str();
-    }
-    inline std::ostream &operator<<(std::ostream &out, const spin &data) noexcept
-    {
-        out << "( " << data.x << " , " << data.y << " , " << data.z << " )";
-        return out;
-    }
-    inline std::istream &operator>>(std::istream &in, spin &data) noexcept
-    {
-        in >> data.x >> data.y >> data.z;
-        return in;
-    }
-    inline std::ostream &operator<<(std::ostream &out, const magn &data) noexcept
-    {
-        out << data.x << "\t" << data.y << "\t" << data.z;
-        return out;
-    }
-    inline std::istream &operator>>(std::istream &in, magn &data) noexcept
-    {
-        in >> data.x >> data.y >> data.z;
-        return in;
-    }
-    inline double abs(const magn &val) noexcept
-    {
-        return std::sqrt(val.x * val.x + val.y * val.y + val.z * val.z);
-    }
+};
+
+inline magn operator+(const spin &lhs, const spin &rhs) noexcept
+{
+    magn result{};
+    result.x = lhs.x + rhs.x;
+    result.y = lhs.y + rhs.y;
+    result.z = lhs.z + rhs.z;
+    return result;
 }
+inline magn operator+(const magn &lhs, const magn &rhs) noexcept
+{
+    magn result{};
+    result.x = lhs.x + rhs.x;
+    result.y = lhs.y + rhs.y;
+    result.z = lhs.z + rhs.z;
+    return result;
+}
+inline magn operator-(const spin &lhs, const spin &rhs) noexcept
+{
+    magn result{};
+    result.x = lhs.x - rhs.x;
+    result.y = lhs.y - rhs.y;
+    result.z = lhs.z - rhs.z;
+    return result;
+}
+inline magn operator-(const magn &lhs, const magn &rhs) noexcept
+{
+    magn result{};
+    result.x = lhs.x - rhs.x;
+    result.y = lhs.y - rhs.y;
+    result.z = lhs.z - rhs.z;
+    return result;
+}
+inline double scalar_multiply(const spin &lhs, const spin &rhs) noexcept
+{
+    double sum = 0.0;
+    sum += lhs.x * rhs.x;
+    sum += lhs.y * rhs.y;
+    sum += lhs.z * rhs.z;
+    return sum;
+}
+inline double scalar_multiply(const magn &lhs, const magn &rhs) noexcept
+{
+    double sum = 0.0;
+    sum += lhs.x * rhs.x;
+    sum += lhs.y * rhs.y;
+    sum += lhs.z * rhs.z;
+    return sum;
+}
+inline magn operator*(const double &lhs, const spin &rhs) noexcept
+{
+    magn result;
+    result.x = lhs * rhs.x;
+    result.y = lhs * rhs.y;
+    result.z = lhs * rhs.z;
+    return (result);
+}
+inline magn operator*(const double &lhs, const magn &rhs) noexcept
+{
+    magn result;
+    result.x = lhs * rhs.x;
+    result.y = lhs * rhs.y;
+    result.z = lhs * rhs.z;
+    return (result);
+}
+inline magn operator*(const spin &lhs, const double &rhs) noexcept
+{
+    return (rhs * lhs);
+}
+inline magn operator*(const magn &lhs, const double &rhs) noexcept
+{
+    return (rhs * lhs);
+}
+inline magn operator/(const spin &lhs, const double &rhs) noexcept
+{
+    magn result{};
+    result.x = lhs.x / rhs;
+    result.y = lhs.y / rhs;
+    result.z = lhs.z / rhs;
+    return result;
+}
+inline magn operator/(const magn &lhs, const double &rhs) noexcept
+{
+    magn result{};
+    result.x = lhs.x / rhs;
+    result.y = lhs.y / rhs;
+    result.z = lhs.z / rhs;
+    return result;
+}
+
+inline bool is_almost_equals(const magn &fst, const magn &snd, double epsilon = 1e-4) noexcept
+{
+    const auto diff = fst - snd;
+    return diff.x < epsilon && diff.y < epsilon && diff.z < epsilon;
+}
+inline std::string to_string(const magn &data) noexcept
+{
+    std::ostringstream stream{};
+    stream << "( " << data.x << " , " << data.y << " , " << data.z << " )";
+    return stream.str();
+}
+inline std::ostream &operator<<(std::ostream &out, const spin &data) noexcept
+{
+    out << "( " << data.x << " , " << data.y << " , " << data.z << " )";
+    return out;
+}
+inline std::istream &operator>>(std::istream &in, spin &data) noexcept
+{
+    in >> data.x >> data.y >> data.z;
+    return in;
+}
+inline std::ostream &operator<<(std::ostream &out, const magn &data) noexcept
+{
+    out << data.x << "\t" << data.y << "\t" << data.z;
+    return out;
+}
+inline std::istream &operator>>(std::istream &in, magn &data) noexcept
+{
+    in >> data.x >> data.y >> data.z;
+    return in;
+}
+inline double abs(const magn &val) noexcept
+{
+    return std::sqrt(val.x * val.x + val.y * val.y + val.z * val.z);
+}
+} // namespace qss::inline models::heisenberg
 #endif
--- src/models/ising.hpp
+++ src/models/ising.hpp
@@ -1,93 +1,98 @@
 #ifndef ISING_HPP_INCLUDED
 #define ISING_HPP_INCLUDED
 
-#include "../random/random.hpp"
 #include "../random/mersenne.hpp"
+#include "../random/random.hpp"
 
-#include <iostream>
-#include <cstdint>
 #include <cmath>
+#include <cstdint>
+#include <iostream>
 
 namespace qss::inline models::ising
 {
-    using magn = double;
-    struct spin
-    {
-        using magn_t = magn;
-        std::int8_t value = 1;
+using magn = double;
+struct spin
+{
+    using magn_t = magn;
+    std::int8_t value = 1;
 
-        template <Random random_t = qss::random::mersenne::random_t<>>
-        static spin generate() noexcept
+    template <Random random_t = qss::random::mersenne::random_t<>> static spin generate() noexcept
+    {
+        static random_t rand{qss::random::get_seed()};
+        auto number = rand(0, 2);
+        if (number == 0)
         {
-            static random_t rand{qss::random::get_seed()};
-            auto number = rand(0, 2);
-            if (number == 0)
-            {
-                number = -1;
-            }
-            return spin{static_cast<std::int8_t>(number)};
+            number = -1;
         }
-
-        operator magn() const noexcept
-        {
-            return static_cast<magn>(value);
-        }
-    };
-
-    inline magn operator+(const spin &lhs, const spin &rhs) noexcept
-    {
-        magn result = lhs.value;
-        result += rhs.value;
-        return result;
-    }
-    inline magn operator-(const spin &lhs, const spin &rhs) noexcept
-    {
-        magn result = lhs.value;
-        result -= rhs.value;
-        return result;
-    }
-    inline double scalar_multiply(const spin &lhs, const spin &rhs) noexcept
-    {
-        magn result = lhs.value;
-        result *= rhs.value;
-        return result;
-    }
-    inline magn operator*(const int &lhs, const spin &rhs) noexcept
-    {
-        magn result = rhs.value;
-        result *= lhs;
-        return result;
-    }
-    inline magn operator*(const double &lhs, const spin &rhs) noexcept
-    {
-        magn result = rhs.value;
-        result *= lhs;
-        return result;
-    }
-    inline magn operator*(const spin &lhs, const double &rhs) noexcept
-    {
-        return (rhs * lhs);
+        return spin{static_cast<std::int8_t>(number)};
     }
 
-    inline std::string to_string(const magn &data) noexcept {
-        std::ostringstream stream{};
-        stream << "( " << data << " )";
-        return stream.str();
-    }
-    inline std::ostream &operator<<(std::ostream &out, const spin &data) noexcept
+    operator magn() const noexcept
     {
-        out << "( " << data.value << " )";
-        return out;
+        return static_cast<magn>(value);
     }
-    inline std::istream &operator>>(std::istream &in, spin &data) noexcept
-    {
-        in >> data.value;
-        return in;
-    }
-    inline double abs(const magn &val) noexcept
-    {
-        return std::abs(val);
-    }
+};
+
+inline magn operator+(const spin &lhs, const spin &rhs) noexcept
+{
+    magn result = lhs.value;
+    result += rhs.value;
+    return result;
 }
+inline magn operator-(const spin &lhs, const spin &rhs) noexcept
+{
+    magn result = lhs.value;
+    result -= rhs.value;
+    return result;
+}
+inline double scalar_multiply(const spin &lhs, const spin &rhs) noexcept
+{
+    magn result = lhs.value;
+    result *= rhs.value;
+    return result;
+}
+inline magn operator*(const int &lhs, const spin &rhs) noexcept
+{
+    magn result = rhs.value;
+    result *= lhs;
+    return result;
+}
+inline magn operator*(const double &lhs, const spin &rhs) noexcept
+{
+    magn result = rhs.value;
+    result *= lhs;
+    return result;
+}
+inline magn operator*(const spin &lhs, const double &rhs) noexcept
+{
+    return (rhs * lhs);
+}
+
+inline bool is_almost_equals(const magn &fst, const magn &snd, double epsilon = 1e-4) noexcept
+{
+    const auto diff = fst - snd;
+    return diff < epsilon;
+}
+inline std::string to_string(const magn &data) noexcept
+{
+    std::ostringstream stream{};
+    stream << "( " << data << " )";
+    return stream.str();
+}
+inline std::ostream &operator<<(std::ostream &out, const spin &data) noexcept
+{
+    out << "( " << data.value << " )";
+    return out;
+}
+inline std::istream &operator>>(std::istream &in, spin &data) noexcept
+{
+    in >> data.value;
+    return in;
+}
+inline double abs(const magn &val) noexcept
+{
+    return std::abs(val);
+}
+} // namespace qss::inline models::ising
 
 #endif
